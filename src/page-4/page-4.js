6f2628b36c07aef79bb9565f6eb257c99834efa9
Bobby Westberg <bellfalasch@gmail.com>
2023-12-21 11:56:07 +0100

    add a little preface to page 4

--- src/page-4/page-4.js
+++ src/page-4/page-4.js
@@ -1,10 +1,5 @@
 /*
 window.onload = function() {
-    const digit = document.createElement('div');
-    digit.className = 'digit';
-    digit.textContent = '15 hours';
-    document.body.appendChild(digit);
-
     const fireworks = document.createElement('div');
     fireworks.className = 'explode';
     document.body.appendChild(fireworks);
